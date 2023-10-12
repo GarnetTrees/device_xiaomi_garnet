984b6615186ca31fcf9f63d1324655cafb64a769
Sonal Singh <sonal.singh.19993@gmail.com>
2023-10-12 18:30:32 +0200

    garnet: prebuilts: Add GoogleCamera (LMC 8.3) - Replace the default SnapCamera with GoogleCamera Signed-off-by: Sensei <senseidev23@gmail.com>

--- device.mk
+++ device.mk
@@ -562,6 +562,10 @@ PRODUCT_PACKAGES += \
     vndservice \
     vndservicemanager
 
+# Gcam
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
 # Verified boot
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
